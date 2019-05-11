f1210f91b134df7eb3369da801ae604110ba48c8
PsycheShaman <gerhard@DESKTOP-FJESODP.22seven.local>
2019-05-11 16:33:04 +0200

    Added 2 feedforward models

--- feedforward_python/model/model2/feed_forward_2.py
+++ feedforward_python/model/model2/feed_forward_2.py
@@ -12,10 +12,14 @@ with open('/scratch/vljchr004/data/msc-thesis-data/x.pkl', 'rb') as x_file:
 
 with open('/scratch/vljchr004/data/msc-thesis-data/y.pkl', 'rb') as y_file:
     y = pickle.load(y_file)
+    
+from tensorflow.keras.utils import to_categorical
+
+y = to_categorical(y)
 
 from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,random_state=123456)
 
 import tensorflow
 
@@ -60,4 +64,26 @@ history = model1.fit(x_train, y_train,
               validation_split=0.15,
               shuffle=True)
 
+import matplotlib.pyplot as plt
+
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('/home/vljchr004/msc-hpc/feedforward_python/fig/feed_forward_2_history.png', bbox_inches='tight')
+
 model1.probs = model1.predict_proba(x_test)
+
+import numpy as np
+np.savetxt("/home/vljchr004/msc-hpc/feedforward_python/results/feed_forward_2_results.csv", np.array(model1.probs), fmt="%s")
