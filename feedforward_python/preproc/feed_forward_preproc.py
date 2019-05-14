09013065257d20005974f30ed692067004e69a37
PsycheShaman <gerhard@DESKTOP-HISBRIN.localdomain>
2019-05-14 17:22:49 +0200

    Run as string

--- feedforward_python/preproc/feed_forward_preproc.py
+++ feedforward_python/preproc/feed_forward_preproc.py
@@ -1,11 +1,11 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("run", help="enter the specific run you need to process",type=int)
+parser.add_argument("run", help="enter the specific run you need to process",type=str)
 args = parser.parse_args()
 
 run = str(args.run)
- 
+
 print("starting")
 
 import glob
@@ -21,7 +21,7 @@ for i in a:
     files_in_order.append(files[i])
 
 print("read files list")
-        
+
 from ast import literal_eval
 
 d = {}
@@ -50,16 +50,16 @@ print("layer and pdg")
 print("get pdg")
 
 def pdg_getter(d,i):
-    
+
     print(str(i))
-    
-    pdgCode_i = d.get(i).get('pdgCode')    
-    
+
+    pdgCode_i = d.get(i).get('pdgCode')
+
     return(pdgCode_i)
 
 import multiprocessing as mp
 
-k = d.keys() 
+k = d.keys()
 
 print("get pdg")
 
@@ -80,15 +80,15 @@ for i in pdgCode:
         electron.append(1)
     else:
         electron.append(0)
-        
+
 print("get layer 0")
-        
+
 def layer0_getter(d,i):
-    
+
     print(str(i))
-    
+
     layer0_i = d.get(i).get('layer 0')
-    
+
     return(layer0_i)
 
 
@@ -100,13 +100,13 @@ layer0 = [pool.apply(layer0_getter, args=(d,i)) for i in k]
 
 pool.close()
 
-        
+
 def layer1_getter(d,i):
-    
+
     print(str(i))
-  
+
     layer1_i = d.get(i).get('layer 1')
-    
+
     return(layer1_i)
 
 
@@ -119,11 +119,11 @@ layer1 = [pool.apply(layer1_getter, args=(d,i)) for i in k]
 pool.close()
 
 def layer2_getter(d,i):
-    
+
     print(str(i))
-  
+
     layer2_i = d.get(i).get('layer 2')
-    
+
     return(layer2_i)
 
 
@@ -136,11 +136,11 @@ layer2 = [pool.apply(layer2_getter, args=(d,i)) for i in k]
 pool.close()
 
 def layer3_getter(d,i):
-    
+
     print(str(i))
-  
+
     layer3_i = d.get(i).get('layer 3')
-    
+
     return(layer3_i)
 
 
@@ -153,11 +153,11 @@ layer3 = [pool.apply(layer3_getter, args=(d,i)) for i in k]
 pool.close()
 
 def layer4_getter(d,i):
-    
+
     print(str(i))
-  
+
     layer4_i = d.get(i).get('layer 4')
-    
+
     return(layer4_i)
 
 
@@ -170,11 +170,11 @@ layer4 = [pool.apply(layer4_getter, args=(d,i)) for i in k]
 pool.close()
 
 def layer5_getter(d,i):
-    
+
     print(str(i))
-  
+
     layer5_i = d.get(i).get('layer 5')
-    
+
     return(layer5_i)
 
 
@@ -192,14 +192,14 @@ print("get x from layer 0")
 
 def x_0_getter(i):
     import numpy as np
-    
+
     layer0 = i
     if type(layer0)==type(None) or np.array(layer0).shape==(17,0):
         pass
     else:
         x0 = np.array(layer0)
         x0 = np.sum(x0,axis=0)
-    
+
     if 'x0' in locals():
         return(x0)
 #
@@ -208,13 +208,13 @@ print("get y from layer 0")
 
 def y_0_getter(electron,i):
     import numpy as np
-    
+
     layer0 = i
     if type(layer0)==type(None) or np.array(layer0).shape==(17,0):
         pass
     else:
         y0 = np.array(electron)
-    
+
     if 'y0' in locals():
         return(y0)
 
@@ -222,20 +222,20 @@ pool = mp.Pool(mp.cpu_count())
 
 x0 = [pool.apply(x_0_getter, args=(i)) for i in (layer0)]
 
-pool.close() 
+pool.close()
 
 
 pool = mp.Pool(mp.cpu_count())
 
 y0 = [pool.apply(y_0_getter, args=(i,electron)) for i in (layer0)]
 
-pool.close() 
+pool.close()
 
 pool = mp.Pool(mp.cpu_count())
 
 x1 = [pool.apply(x_0_getter, args=(i)) for i in (layer1)]
 
-pool.close() 
+pool.close()
 
 
 pool = mp.Pool(mp.cpu_count())
@@ -248,53 +248,53 @@ pool = mp.Pool(mp.cpu_count())
 
 x2 = [pool.apply(x_0_getter, args=(i)) for i in (layer2)]
 
-pool.close() 
+pool.close()
 
 
 pool = mp.Pool(mp.cpu_count())
 
 y2 = [pool.apply(y_0_getter, args=(electron,i)) for i in (layer2)]
 
-pool.close() 
+pool.close()
 
 pool = mp.Pool(mp.cpu_count())
 
 x3 = [pool.apply(x_0_getter, args=(i)) for i in (layer3)]
 
-pool.close() 
+pool.close()
 
 
 pool = mp.Pool(mp.cpu_count())
 
 y3 = [pool.apply(y_0_getter, args=(electron,i)) for i in (layer3)]
 
-pool.close() 
+pool.close()
 
 pool = mp.Pool(mp.cpu_count())
 
 x4 = [pool.apply(x_0_getter, args=(i)) for i in (layer4)]
 
-pool.close() 
+pool.close()
 
 
 pool = mp.Pool(mp.cpu_count())
 
 y4 = [pool.apply(y_0_getter, args=(electron,i)) for i in (layer4)]
 
-pool.close() 
+pool.close()
 
 pool = mp.Pool(mp.cpu_count())
 
 x5 = [pool.apply(x_0_getter, args=(i)) for i in (layer5)]
 
-pool.close() 
+pool.close()
 
 
 pool = mp.Pool(mp.cpu_count())
 
 y5 = [pool.apply(y_0_getter, args=(electron,i)) for i in (layer5)]
 
-pool.close() 
+pool.close()
 
 #
 
@@ -315,34 +315,12 @@ x /= mu
 print("pickling files")
 
 import pickle
- 
+
 with open('/scratch/vljchr004/data/msc-thesis-data/x.pkl', 'wb') as x_file:
   pickle.dump(x, x_file)
-  
+
 with open('/scratch/vljchr004/msc-thesis-data/y.pkl', 'wb') as y_file:
   pickle.dump(y, y_file)
 
 
 print("done.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
