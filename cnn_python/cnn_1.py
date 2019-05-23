bf0ade04fe6ac2da712abd087b354d9d11799f85
PsycheShaman <gerhard@DESKTOP-HISBRIN.22seven.local>
2019-05-23 22:27:57 +0200

    cat pickles separate no more

--- cnn_python/cnn_1.py
+++ cnn_python/cnn_1.py
@@ -8,12 +8,76 @@ Created on Wed May 22 18:01:37 2019
 print("==============================================================================================")
 
 import pickle
- 
-with open('/scratch/vljchr004/data/msc-thesis-data/cnn/x.pkl', 'rb') as x_file:
-    x = pickle.load(x_file)
 
-with open('/scratch/vljchr004/data/msc-thesis-data/cnn/y.pkl', 'rb') as y_file:
-    y = pickle.load(y_file)
+import glob
+
+import numpy as np
+
+x_files = glob.glob("/scratch/vljchr004/data/msc-thesis-data/cnn/x_*.pkl")
+y_files = glob.glob("/scratch/vljchr004/data/msc-thesis-data/cnn/y_*.pkl")
+
+#x_files = glob.glob("C:/Users/gerhard/Documents/msc-thesis-data/cnn/x_*.pkl")
+#y_files = glob.glob("C:/Users/gerhard/Documents/msc-thesis-data/cnn/y_*.pkl")
+
+print("loading first x pickle........................................................................................")
+
+with open(x_files[0], 'rb') as x_file0:
+    x = pickle.load(x_file0)
+    
+print("loading first y pickle........................................................................................")
+
+with open(y_files[0], 'rb') as y_file0:
+   y = pickle.load(y_file0)
+   
+print("recursively adding x pickles........................................................................................")
+
+#for i in x_files[1:]:
+for i in x_files[1:2]:
+    with open(i,'rb') as x_file:
+        xi = pickle.load(x_file)
+        x = np.concatenate((x,xi),axis=0)
+        
+print("recursively adding y pickles........................................................................................")
+        
+#for i in y_files[1:]:
+for i in y_files[1:2]:
+    with open(i,'rb') as y_file:
+        yi = pickle.load(y_file)
+        y = np.concatenate((y,yi),axis=None)
+
+#remove 0 elements
+    
+nz = np.array([np.count_nonzero(i) for i in x])
+
+zeros = np.where(nz==0)
+
+x = np.delete(x,zeros,axis=0)
+y = np.delete(y,zeros)
+
+#oversample electrons
+
+elec = np.where(y==1)
+pion = np.where(y!=1)
+
+int(elec[0].shape[0])/int(pion[0].shape[0])
+
+electrons_x = x[elec,:,:]
+
+electrons_y = y[elec]
+
+electrons_x = np.squeeze(electrons_x)
+
+x = np.concatenate((electrons_x,x,electrons_x),axis=0)
+
+y = np.concatenate((electrons_y,y,electrons_y),axis=None)
+
+x = x.reshape(x.shape[0],x.shape[1],x.shape[2],1)
+ 
+#with open('/scratch/vljchr004/data/msc-thesis-data/cnn/x.pkl', 'rb') as x_file:
+#    x = pickle.load(x_file)
+#
+#with open('/scratch/vljchr004/data/msc-thesis-data/cnn/y.pkl', 'rb') as y_file:
+#    y = pickle.load(y_file)
 
 #with open('C:/Users/gerhard/Documents/msc-thesis-data/cnn/x_000265309.pkl', 'rb') as x_file:
 #    x = pickle.load(x_file)
@@ -21,7 +85,7 @@ with open('/scratch/vljchr004/data/msc-thesis-data/cnn/y.pkl', 'rb') as y_file:
 #with open('C:/Users/gerhard/Documents/msc-thesis-data/cnn/y_000265309.pkl', 'rb') as y_file:
 #    y = pickle.load(y_file)
     
-x = x.reshape(x.shape[0],x.shape[1],x.shape[2],1)
+#x = x.reshape(x.shape[0],x.shape[1],x.shape[2],1)
     
 
 from tensorflow.keras.utils import to_categorical
@@ -96,7 +160,6 @@ plt.savefig('/home/vljchr004/msc-hpc/cnn_python/fig/cnn_1_history2.png', bbox_in
 
 model.probs = model.predict_proba(x_test)
 
-import numpy as np
 np.savetxt("/home/vljchr004/msc-hpc/cnn_python/results/cnn_1_results.csv", np.array(model.probs), fmt="%s")
 
 #np.savetxt("C:/Users/gerhard/Documents/msc-hpc/cnn_python/results/test/cnn_1_results.csv", np.array(model.probs), fmt="%s")
@@ -110,6 +173,8 @@ model.save('/home/vljchr004/msc-hpc/cnn_python/cnn1.h5')  # creates a HDF5 file 
 #model.save('C:/Users/gerhard/Documents/msc-hpc/cnn_python/results/test/cnn1.h5')
 
 del model
+
+print("<----*********************+++++++++done+++++++++++*******************---->")
 
 
 
